957233c812620295bcf8d9a8f6b192e5588a3a73
kerklangsi <arifilah.01@gmail.com>
2025-06-20 19:42:20 +0800

    Update mq4_chart.js

--- mq4_chart.js
+++ mq4_chart.js
@@ -65,7 +65,7 @@ function updateMQ4Chart() {
     chart: { type: 'spline', reflow: true, spacingRight: 10 },
     title: { text: 'MQ4 PPM Sensor Data' },
     xAxis: { type: 'datetime', title: { text: 'Time' }, min: timeRangeMQ4 === 'max' ? null : fromTime, max: timeRangeMQ4 === 'max' ? null : now },
-    yAxis: { title: { text: 'PPM' }, min: 0, max: 1000 },
+    yAxis: { title: { text: 'PPM' }, min: 0, max: 10000 },
     tooltip: { xDateFormat: '%Y-%m-%d %H:%M:%S', valueSuffix: ' PPM' },
     legend: { layout: 'vertical', align: 'right', verticalAlign: 'top', floating: true, borderWidth: 1, backgroundColor: '#FFFFFF' },
     series: [{ name: 'MQ4 PPM', data: points, color: '#e60909' }]
